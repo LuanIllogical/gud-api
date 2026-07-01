e0943d7cd3c53ae762a6d51fe7f90a8bac662516
LuanIllogical <60335462+LuanIllogical@users.noreply.github.com>
2026-07-01 15:30:49 -0300

    fix: readding cors

--- api/dossier.js
+++ api/dossier.js
@@ -388,7 +388,7 @@ async function findVideoFiles(username, path = '') {
 
 module.exports = async (req, res) => {
     try {
-        res.setHeader("Access-Control-Allow-Origin", "*"); //https://luanillogical.github.io
+        res.setHeader("Access-Control-Allow-Origin", "https://luanillogical.github.io");
         res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
